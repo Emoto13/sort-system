91e117d1bf8fc269003da24523392194c799a0b5
Emoto13 <emcho_spassov@abv.bg>
2021-06-26 16:18:34 +0300

    Update state

--- fulfillment-service/state/state_manager.go
+++ fulfillment-service/state/state_manager.go
@@ -1,6 +1,7 @@
 package state
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/Emoto13/sort-system/gen"
@@ -8,64 +9,137 @@ import (
 )
 
 type State interface {
-	
+	GetCubbyByItemCode(itemCode string) (*gen.Cubby, error)
+	GetPreparedOrders(orders []*gen.Order) []*gen.PreparedOrder
+	GetOrderStateById(orderId string) (gen.OrderState, error)
+	SetOrdersState(orders []*gen.Order, status gen.OrderState) error
+	SetOrderStateById(orderId string, status gen.OrderState) error
+	GetFulfillmentStatusByOrderId(orderId string) ([]*gen.FulfillmentStatus, error)
+	GetFulfillmentStatusOfAllOrders() ([]*gen.FulfillmentStatus, error)
+	Clear()
 }
 
-type StateManager struct {
-	ItemCodeToCubbyIds map[string][]*gen.Cubby
-	CubbyIdToOrderId   map[string]string
-	NumberOfItems      int
-	mu                 sync.Mutex
+type stateManager struct {
+	ItemCodeToCubby  map[string][]*gen.Cubby
+	CubbyIdToOrderId map[string]string
+	OrderIdToCubby   map[string]*gen.Cubby
+	OrderIdToItems   map[string][]*gen.Item
+	OrderIdToState   map[string]gen.OrderState
+	mu               sync.RWMutex
 }
 
-func NewStateManager() *StateManager {
-	return &StateManager{
-		ItemCodeToCubbyIds: make(map[string][]*gen.Cubby),
-		CubbyIdToOrderId:   make(map[string]string),
-		NumberOfItems:      0,
-		mu:                 sync.Mutex{},
+func New() State {
+	return &stateManager{
+		ItemCodeToCubby:  make(map[string][]*gen.Cubby),
+		CubbyIdToOrderId: make(map[string]string),
+		OrderIdToCubby:   make(map[string]*gen.Cubby),
+		OrderIdToItems:   make(map[string][]*gen.Item),
+		OrderIdToState:   make(map[string]gen.OrderState),
+		mu:               sync.RWMutex{},
 	}
 }
 
-func (sm *StateManager) mapItemCodesToCubby(items []*gen.Item, cubby *gen.Cubby) {
+func (sm *stateManager) mapItemCodesToCubby(items []*gen.Item, cubby *gen.Cubby) {
 	for _, item := range items {
-		sm.ItemCodeToCubbyIds[item.Code] = append(sm.ItemCodeToCubbyIds[item.Code], cubby)
+		sm.ItemCodeToCubby[item.Code] = append(sm.ItemCodeToCubby[item.Code], cubby)
 	}
 }
 
-func (sm *StateManager) getCubbyIdByOrderId(orderId string, times int) string {
+func (sm *stateManager) getCubbyIdByOrderId(orderId string, times int) string {
 	cubbyId := ordertocubby.Map(orderId, uint32(times), 10)
 	attemptsToAvoidCollision := 1
 	for true {
-		if _, present := sm.CubbyIdToOrderId[cubbyId]; !present {
+		if _, ok := sm.CubbyIdToOrderId[cubbyId]; !ok {
 			break
 		}
+
 		cubbyId = ordertocubby.Map(orderId, uint32(times+attemptsToAvoidCollision), 10)
 		attemptsToAvoidCollision++
 	}
 	return cubbyId
 }
 
-func (sm *StateManager) GetPreparedOrders(orders []*gen.Order) []*gen.PreparedOrder {
+func (sm *stateManager) GetPreparedOrders(orders []*gen.Order) []*gen.PreparedOrder {
 	preparedOrders := []*gen.PreparedOrder{}
 
 	for i, order := range orders {
+		sm.OrderIdToItems[order.Id] = order.Items
+
 		cubbyId := sm.getCubbyIdByOrderId(order.Id, i)
 		sm.CubbyIdToOrderId[cubbyId] = order.Id
 
 		cubby := &gen.Cubby{Id: cubbyId}
+		sm.OrderIdToCubby[order.Id] = cubby
+
 		preparedOrder := &gen.PreparedOrder{Order: order, Cubby: cubby}
 		preparedOrders = append(preparedOrders, preparedOrder)
 
 		sm.mapItemCodesToCubby(order.Items, cubby)
-		sm.NumberOfItems += len(order.Items)
 	}
 
 	return preparedOrders
 }
 
-func (sm *StateManager) GetCubbyByItemCode(itemCode string) *gen.Cubby {
-	cubby := sm.ItemCodeToCubbyIds[itemCode][0]
-	sm.ItemCodeToCubbyIds[itemCode] = sm.ItemCodeToCubbyIds[itemCode][1:]
-	return cubby
+func (sm *stateManager) GetCubbyByItemCode(itemCode string) (*gen.Cubby, error) {
+	if len(sm.ItemCodeToCubby[itemCode]) == 0 {
+		return nil, fmt.Errorf("This item was distributed to all necessary cubbies")
+	}
+
+	cubby := sm.ItemCodeToCubby[itemCode][0]
+	sm.ItemCodeToCubby[itemCode] = sm.ItemCodeToCubby[itemCode][1:]
+	return cubby, nil
+}
+
+func (sm *stateManager) Clear() {
+	sm.ItemCodeToCubby = map[string][]*gen.Cubby{}
+	sm.CubbyIdToOrderId = map[string]string{}
+	sm.OrderIdToCubby = map[string]*gen.Cubby{}
+	sm.OrderIdToItems = map[string][]*gen.Item{}
+	sm.OrderIdToState = map[string]gen.OrderState{}
+}
+
+func (sm *stateManager) GetOrderStateById(orderId string) (gen.OrderState, error) {
+	orderState, ok := sm.OrderIdToState[orderId]
+	if !ok {
+		return gen.OrderState_FAILED, fmt.Errorf("No order with such ID")
+	}
+
+	return orderState, nil
+}
+
+func (sm *stateManager) SetOrdersState(orders []*gen.Order, state gen.OrderState) error {
+	for _, order := range orders {
+		sm.OrderIdToState[order.Id] = state
+	}
+	return nil
+}
+
+func (sm *stateManager) SetOrderStateById(orderId string, state gen.OrderState) error {
+	sm.OrderIdToState[orderId] = state
+	return nil
+}
+
+func (sm *stateManager) GetFulfillmentStatusByOrderId(orderId string) ([]*gen.FulfillmentStatus, error) {
+	order := &gen.Order{Id: orderId, Items: sm.OrderIdToItems[orderId]}
+	orderState, err := sm.GetOrderStateById(order.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	fulfillmentStatus := &gen.FulfillmentStatus{Order: order, Cubby: sm.OrderIdToCubby[orderId], State: orderState}
+	return []*gen.FulfillmentStatus{fulfillmentStatus}, nil
+}
+
+func (sm *stateManager) GetFulfillmentStatusOfAllOrders() ([]*gen.FulfillmentStatus, error) {
+	fulfillmentStatusSlice := []*gen.FulfillmentStatus{}
+
+	for orderId, _ := range sm.OrderIdToItems {
+		fulfillmentStatus, err := sm.GetFulfillmentStatusByOrderId(orderId)
+		if err != nil {
+			return nil, err
+		}
+
+		fulfillmentStatusSlice = append(fulfillmentStatusSlice, fulfillmentStatus[0])
+	}
+	return fulfillmentStatusSlice, nil
 }
