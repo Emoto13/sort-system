2e5cbc21ed628a7da3c7cb91834c781643fc8c3f
Emoto13 <emcho_spassov@abv.bg>
2021-06-29 15:46:26 +0300

    Update service

--- fulfillment-service/service/service.go
+++ fulfillment-service/service/service.go
@@ -7,33 +7,40 @@ import (
 	"sync"
 
 	"github.com/Emoto13/sort-system/fulfillment-service/state"
-
 	"github.com/Emoto13/sort-system/gen"
 )
 
 type fulfillmentService struct {
-	//orderProcessor     orderproc.OrderProcessor
-	sortingRobot            gen.SortingRobotClient
-	state                   state.State
-	orders                  chan []*gen.Order
-	areOrdersBeingProcessed bool
-	mu                      *sync.Mutex
+	sortingRobot     gen.SortingRobotClient
+	state            state.State
+	orders           chan []*gen.Order
+	processingOrders bool
+	mu               sync.Mutex
 }
 
-func NewFulfillmentService(sortingRobot gen.SortingRobotClient) gen.FulfillmentServer {
+func New(sortingRobot gen.SortingRobotClient) gen.FulfillmentServer {
 	return &fulfillmentService{
-		//orderProcessor:     orderproc.New(sortingRobot),
-		sortingRobot:            sortingRobot,
-		state:                   state.New(),
-		orders:                  make(chan []*gen.Order),
-		areOrdersBeingProcessed: false,
-		mu:                      &sync.Mutex{},
+		sortingRobot:     sortingRobot,
+		state:            state.New(),
+		orders:           make(chan []*gen.Order),
+		processingOrders: false,
+		mu:               sync.Mutex{},
 	}
+}
+
+func (fs *fulfillmentService) areOrdersBeingProcessed() bool {
+	return fs.processingOrders
+}
+
+func (fs *fulfillmentService) setAreOrdersBeingProcessed(value bool) {
+	fs.mu.Lock()
+	defer fs.mu.Unlock()
+	fs.processingOrders = value
 }
 
 func (fs *fulfillmentService) LoadOrders(ctx context.Context, in *gen.LoadOrdersRequest) (*gen.CompleteResponse, error) {
 	fs.orders <- in.Orders
-	if fs.areOrdersBeingProcessed {
+	if fs.areOrdersBeingProcessed() {
 		return &gen.CompleteResponse{Status: "Will start to process the request shortly", Orders: []*gen.PreparedOrder{}}, nil
 	}
 
@@ -43,32 +50,52 @@ func (fs *fulfillmentService) LoadOrders(ctx context.Context, in *gen.LoadOrders
 func (fs *fulfillmentService) ProcessOrders(ctx context.Context, in *gen.Empty) (*gen.Empty, error) {
 	for {
 		orders := <-fs.orders
-		fs.areOrdersBeingProcessed = true
-		//fs.orderProcessor.ProcessOrders(ctx, orders)
-		fs.StartProcessingOrder(ctx, orders)
-		fs.areOrdersBeingProcessed = false
+
+		if !fs.areOrdersBeingProcessed() {
+			fs.setAreOrdersBeingProcessed(true)
+			fs.StartProcessingOrder(ctx, orders)
+		}
+
+		fs.setAreOrdersBeingProcessed(false)
 	}
 	return &gen.Empty{}, nil
 }
 
-func (fs *fulfillmentService) StartProcessingOrder(ctx context.Context, orders []*gen.Order) []*gen.PreparedOrder {
-	fs.mu.Lock()
-	defer fs.mu.Unlock()
+func (fs *fulfillmentService) StartProcessingOrder(ctx context.Context, orders []*gen.Order) ([]*gen.PreparedOrder, error) {
 	fmt.Println("Start Processing Order")
 	fs.state.Clear()
-	fs.state.SetOrdersState(orders, gen.OrderState_PENDING)
+	fs.state.AddOrders(orders)
 
-	preparedOrders := fs.state.GetPreparedOrders(orders)
-	err := fs.fulfillOrders(ctx, orders)
+	preparedOrders, err := fs.GetPreparedOrders(orders)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 
-	return preparedOrders
+	err = fs.fulfillOrders(ctx, orders)
+	if err != nil {
+		return nil, err
+	}
+
+	return preparedOrders, nil
 }
 
-func (fs *fulfillmentService) fulfillOrders(ctx context.Context, preparedOrders []*gen.Order) error {
-	for _, order := range preparedOrders {
+func (fs *fulfillmentService) GetPreparedOrders(orders []*gen.Order) ([]*gen.PreparedOrder, error) {
+	preparedOrders := []*gen.PreparedOrder{}
+	for _, order := range orders {
+		cubby, err := fs.state.GetOrderCubby(order.Id)
+		if err != nil {
+			return nil, err
+		}
+
+		preparedOrder := &gen.PreparedOrder{Order: order, Cubby: cubby}
+		preparedOrders = append(preparedOrders, preparedOrder)
+	}
+
+	return preparedOrders, nil
+}
+
+func (fs *fulfillmentService) fulfillOrders(ctx context.Context, orders []*gen.Order) error {
+	for _, order := range orders {
 		for _, _ = range order.Items {
 			resp, err := fs.sortingRobot.SelectItem(ctx, &gen.Empty{})
 			if err != nil {
@@ -88,32 +115,44 @@ func (fs *fulfillmentService) fulfillOrders(ctx context.Context, preparedOrders 
 
 			fmt.Println("Item with code ", resp.Item.Code, " is moved to: ", cubby.Id)
 		}
-		fs.state.SetOrderStateById(order.Id, gen.OrderState_READY)
+		fs.state.SetOrderStatus(order.Id, gen.OrderStatus_READY)
 	}
 
 	return nil
 }
 
-func (fs *fulfillmentService) GetOrderStatusById(ctx context.Context, in *gen.OrderIdRequest) (*gen.OrdersStatusResponse, error) {
-	fulfillmentStatus, err := fs.state.GetFulfillmentStatusByOrderId(in.OrderId)
+func (fs *fulfillmentService) GetOrderFulfillmentStatusById(ctx context.Context, in *gen.OrderIdRequest) (*gen.OrdersStatusResponse, error) {
+	items, err := fs.state.GetOrderItems(in.OrderId)
 	if err != nil {
 		return nil, err
 	}
 
-	return &gen.OrdersStatusResponse{Status: fulfillmentStatus}, nil
+	cubby, err := fs.state.GetOrderCubby(in.OrderId)
+	if err != nil {
+		return nil, err
+	}
+
+	orderStatus, err := fs.state.GetOrderStatus(in.OrderId)
+	if err != nil {
+		return nil, err
+	}
+
+	order := &gen.Order{Id: in.OrderId, Items: items}
+	fulfillmentStatus := &gen.FulfillmentStatus{Order: order, Cubby: cubby, Status: orderStatus}
+	return &gen.OrdersStatusResponse{FulfillmentStatus: []*gen.FulfillmentStatus{fulfillmentStatus}}, nil
 }
 
-func (fs *fulfillmentService) GetAllOrdersStatus(ctx context.Context, in *gen.Empty) (*gen.OrdersStatusResponse, error) {
+func (fs *fulfillmentService) GetAllOrdersFulfillmentStatus(ctx context.Context, in *gen.Empty) (*gen.OrdersStatusResponse, error) {
 	allOrdersFulfillmentStatus, err := fs.state.GetFulfillmentStatusOfAllOrders()
 	if err != nil {
 		return nil, err
 	}
 
-	return &gen.OrdersStatusResponse{Status: allOrdersFulfillmentStatus}, nil
+	return &gen.OrdersStatusResponse{FulfillmentStatus: allOrdersFulfillmentStatus}, nil
 }
 
 func (fs *fulfillmentService) MarkFulfilled(ctx context.Context, in *gen.OrderIdRequest) (*gen.Empty, error) {
-	err := fs.state.SetOrderStateById(in.OrderId, gen.OrderState_READY)
+	err := fs.state.SetOrderStatus(in.OrderId, gen.OrderStatus_READY)
 	if err != nil {
 		return nil, err
 	}
